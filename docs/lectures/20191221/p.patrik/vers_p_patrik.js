949f74b2aa284b64c1274f5a862605d7dbc5f951
Gergely Kovacs <gergely.kovacs02@gmail.com>
2019-12-21 16:02:32 +0100

    second part

--- docs/lectures/20191221/p.patrik/vers_p_patrik.js
+++ docs/lectures/20191221/p.patrik/vers_p_patrik.js
@@ -18,4 +18,25 @@ let a = versString.split("\n")
 
 }
 
-console.log(oddLines('../vers.txt'));
+function evenLines(path){
+
+  let vers = fs.readFileSync(path);
+  let versString = vers.toString();
+  let a = versString.split("\n")
+  
+      let array = []  
+        for (let index = 0; index < a.length-1; index++) {
+            if(index%2 !== 0){
+               
+             array.push(a[index]);
+            } 
+        }
+  
+      return array;
+  
+  }
+
+module.exports = {
+  oddLines: oddLines,
+  evenLines: evenLines
+}
