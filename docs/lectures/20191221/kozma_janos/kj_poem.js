9b7832a04f1c0d06eb21cdd8947afe484631e2ee
Kozma János <41972874+JKozma81@users.noreply.github.com>
2019-12-21 14:25:25 +0100

    initial commit

---
+++ docs/lectures/20191221/kozma_janos/kj_poem.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+function oddLines(path) {
+	const poemData = fs.readFileSync(path).toString();
+	const poemArray = poemData.split('\r\n');
+
+	return poemArray.filter((line, idx) => idx % 2);
+}
+
+const poem_file = process.argv[2];
