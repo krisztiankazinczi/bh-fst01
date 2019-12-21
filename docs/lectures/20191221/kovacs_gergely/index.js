304bc14850aa1c06895b9d3a766473d13aa19413
Krisz <krisz@example.com>
2019-12-21 14:49:16 +0100

    evenLines function

--- docs/lectures/20191221/kovacs_gergely/index.js
+++ docs/lectures/20191221/kovacs_gergely/index.js
@@ -11,10 +11,22 @@ function oddLines(path) {
     }
 }
 
-let vers = 'docs/lectures/20191221/vers.txt';
+let vers = '../vers.txt';
 
+function evenLines(path) {
+    const fileBuffer = fs.readFileSync(path);
+    const poem = fileBuffer.toString();
+    let oddpoem = poem.split('\n');
+    let arr = [];
+    for(let i = 0; i < oddpoem.length; i++){
+        if(i%2 ===0){
+            arr.push(oddpoem[i]);
+        }
+    }
+    return arr;
+}
 
-oddLines(vers);
-
+let result = evenLines(vers);
+console.log(result);
 
 
