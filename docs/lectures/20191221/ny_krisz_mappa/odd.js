300a02f4154af824335cbef5a45bb43cffbaa740
Kozma János <41972874+JKozma81@users.noreply.github.com>
2019-12-21 15:12:11 +0100

    added evenLines

--- docs/lectures/20191221/ny_krisz_mappa/odd.js
+++ docs/lectures/20191221/ny_krisz_mappa/odd.js
@@ -1,15 +1,21 @@
-const fs = require('fs')
+const fs = require('fs');
 
+function oddLines(path) {
+	let output = [];
+	let buffer = fs.readFileSync(path);
+	content = buffer.toString().split('\n');
 
-function oddLines(path){
-    let output = []
-    let buffer = fs.readFileSync(path)
-    content = buffer.toString().split('\n')
-
-    for (let i = 0; i < content.length; i+=2) {
-        output.push(content[i])
-    }
-    console.log(output)
+	for (let i = 0; i < content.length; i += 2) {
+		output.push(content[i]);
+	}
+	return output;
 }
 
-oddLines('../vers.txt')
+function evenLines(path) {
+	const poemData = fs.readFileSync(path).toString();
+	const poemArray = poemData.split('\r\n');
+
+	return poemArray.filter((line, idx) => idx % 2);
+}
+
+oddLines('../vers.txt');
