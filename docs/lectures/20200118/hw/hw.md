4ad182c7484eb942cc5790f256b924c5732b43cd
icsaba <csaba.ivancza@gmail.com>
2020-01-18 18:00:43 +0100

    adding hw

---
+++ docs/lectures/20200118/hw/hw.md
@@ -0,0 +1,11 @@
+# Homework
+
+legyen a hazi a 6os feladat.
+
+## summary
+ - legyen egy form a user adatok bevitelere amely csinal egy postot
+    -   username, email, password
+ - post route az adatok feldolgozasara
+ - mentsuk el az adatokat a mar meglevo adatbazisba
+ - a /users-el tudjuk elerni a rekordokat
+ - opcionalis, html oldalt csinalni user rekordok mutatasara
--- docs/lectures/20200118/users/index.js
+++ docs/lectures/20200118/users/index.js
@@ -8,11 +8,45 @@ const port = 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+function getUsersFromDB(callback) {
+    db.serialize(() => {
+        db.all("SELECT * FROM users", (err, rows) => {
+            callback(rows);
+        });
+    });
+}
+
+app.get('/users', (req, res) => {
+    
+    getUsersFromDB( (users) => {
+        // todod table here
+       res.send(users);
+
+    });
+
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 
-    db.serialize(function() {
-        db.run("CREATE TABLE result (value TEXT)");
+    db.serialize(() => {
+        db.run("CREATE TABLE users (username TEXT, email TEXT, password TEXT)");
+
+        const prep = db.prepare('INSERT into users VALUES (?, ?, ?)');
+
+        const users = [
+            ['csaba', 'csaba@test.com', 'holmi'],
+            ['valakimas', 'valaki@huha.com', 'titkosjelszo']
+        ];
+
+        users.forEach( user => prep.run(...user) );
+        prep.finalize();
+
+        db.each("SELECT * FROM users", (err, row) => {
+            console.log(`username ${row.username}`);
+        });
+
     });
 
     // db.close();
